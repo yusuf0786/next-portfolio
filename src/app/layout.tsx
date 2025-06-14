97fb355db0d9bd8cb309c8b28185bb2fc45a1b56
yusuf0786 <yusssuf0786@gmail.com>
2025-06-15 00:51:57 +0530

    global layout.tsx fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,10 +24,10 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
   modal,
-}: Readonly<{
+}:{
   children: React.ReactNode;
   modal: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <head>
